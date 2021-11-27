a47a52d599c324fddb34566d7f885a80af55fa25
mariem rabhi <rab.mariem@gmail.com>
2021-11-27 15:04:58 +0100

    new style

--- src/app/Application/list-voyages/list-voyages.component.ts
+++ src/app/Application/list-voyages/list-voyages.component.ts
@@ -2,8 +2,10 @@
 import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { Voyage } from 'src/app/Classes/voyage';
 import { VoyageService } from 'src/app/Services/voyage.service';
+import {filter, map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-voyages',
@@ -12,17 +14,30 @@ import { VoyageService } from 'src/app/Services/voyage.service';
 })
 export class ListVoyagesComponent implements OnInit {
   ListeVoyages: Voyage[] = [];
+  libVoyages:string[]=[];
   voyageForm: FormGroup = new FormGroup({});
   problemeForm: FormGroup = new FormGroup({});
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
   fourthFormGroup: FormGroup;
+  filteredOptions: Observable<string[]>;
+
   constructor(private voyageService: VoyageService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.ListeVoyages = this.voyageService.getVoyages();
 
+    // var i=0;
+    // for (let p of this.ListeVoyages)
+    // {
+    //   this.libVoyages[i]=p.pays;
+    //   console.log(this.libVoyages[i]);
+    //   i++;
+    // }
+    // this.filteredOptions = this.voyageForm.valueChanges.pipe(
+    //   startWith(''),map(libVoyages => this._filter(libVoyages)),
+    // );
     this.voyageForm = this.fb.group({
       datedep:[new Date()],
       datedarr:[new Date()],
@@ -48,6 +63,12 @@ export class ListVoyagesComponent implements OnInit {
       fourthCtrl: ['']
     });
   }
+//   private _filter(value: string): string[] {
+//     const filterValue = value.toLowerCase();
+// console.log(filterValue)
+//     return this.libVoyages.filter(option => option.toLowerCase().includes(filterValue));
+//   }
+
   paysc(): boolean {
     return this.voyageForm.controls.paysc.value
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {MatFormFieldModule } from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
     MatFormFieldModule,
     MatButtonModule,
     MatSelectModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
